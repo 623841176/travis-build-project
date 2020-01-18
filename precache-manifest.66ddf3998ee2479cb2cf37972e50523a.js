d362494bbf2f26c344f5ba8560c02e38f4a80b7b
wufei.chen (from Travis CI) <623841176@qq.com>
2020-01-18 07:30:02 +0000

    Deploy 623841176/travis-build-project to github.com/623841176/travis-build-project.git:gh-pages

---
+++ precache-manifest.66ddf3998ee2479cb2cf37972e50523a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6e2db3189b60cabd696172e5df332d",
+    "url": "/travis-build-project/index.html"
+  },
+  {
+    "revision": "64cc0d31c15132240bc3",
+    "url": "/travis-build-project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "53ae2b659f09b5581dc7",
+    "url": "/travis-build-project/static/js/2.e8af3f03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/travis-build-project/static/js/2.e8af3f03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64cc0d31c15132240bc3",
+    "url": "/travis-build-project/static/js/main.25ae2d58.chunk.js"
+  },
+  {
+    "revision": "ec1d49a5eb2378e41521",
+    "url": "/travis-build-project/static/js/runtime-main.a882b858.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travis-build-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
